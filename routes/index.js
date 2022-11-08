10f5c71149f10d4125a20a510015c30c88729ecf
mecaniser <sergioburca@gmail.com>
2022-11-08 01:19:52 -0500

    update landing page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Books = require('../model/book')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Books.find().sort({createdAt: 'desc'}).limit(3).exec()
+    res.render('index', { books })
+  } catch (error) {
+    books = []
+  }
 })
 
 module.exports = router
